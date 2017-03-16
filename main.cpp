9af1f507e846b896683027078cfe2c7d3f5ac56a
dmy <dmy@debian.debian>
2017-03-16 20:42:26 +0800

    simple echo func

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include <iostream>
 #include <sys/epoll.h>
 #define MAX_QUEUE_SIZE 1024 * 1024 * 64
-#define MAX_EPOLL_EVENT 128 
+#define MAX_EPOLL_EVENT 128
 struct transform_queue_t
 {
     pthread_mutex_t mutex;
@@ -69,8 +69,25 @@ struct transform_queue_t
     }
 };
 
+struct transform_cmd_t
+{
+    uint8_t cmd_type = 0;
+    uint32_t cmd_size = 0;
+};
+
+
+//TODO(dmy) 不应该使用sock-fd，使用自己生成的唯一标志id
+struct transform_cmd_socket_data_t : transform_cmd_t
+{
+    int sock_fd;
+    uint32_t data_len;
+    unsigned char* data[0];
+};
+
+
 transform_queue_t* p_2_c_queue;
 transform_queue_t* c_2_p_queue;
+int pipefd[2];
 
 void parent();
 void child();
@@ -82,6 +99,8 @@ int main()
         PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANON, 0, 0);
     p_2_c_queue->init();
     c_2_p_queue->init();
+
+    pipe2(pipefd, O_NONBLOCK);
 
     int pid = fork();
     if (pid) {
@@ -122,7 +141,10 @@ int get_server_socket(const std::string& addr, int port)
 
 void parent()
 {
-    void* read_buffer = malloc(MAX_QUEUE_SIZE);
+    void* pipe_discard_buffer = malloc(1024);
+    void* read_buffer = malloc(MAX_QUEUE_SIZE * 2);
+    void* write_buffer = malloc(MAX_QUEUE_SIZE * 2);
+
     int server_fd = get_server_socket("127.0.0.1", 8890);
 
     struct epoll_event ev, events[MAX_EPOLL_EVENT];
@@ -131,48 +153,88 @@ void parent()
     ev.data.fd = server_fd;
     epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev);
 
+    ev.events = EPOLLIN;
+    ev.data.fd = pipefd[0];
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pipefd[0], &ev);
+
     int nfds = 0;
     for (;;) {
-        nfds = epoll_wait(epoll_fd, events, MAX_EPOLL_EVENT, 1);//1ms的超时，做一些定时操作
+        nfds = epoll_wait(epoll_fd, events, MAX_EPOLL_EVENT, -1);//可以设置1ms的超时，做一些定时操作
         for (int n = 0; n < nfds; ++n) {
             if (events[n].data.fd == server_fd) {
                 int client = accept(server_fd, nullptr, nullptr);
-                fcntl(client, F_SETFL, O_NONBLOCK);
+                fcntl(client, F_SETFL, O_NONBLOCK);//TODO(dmy) 是否设置nodelay？
                 ev.events = EPOLLIN;
                 ev.data.fd = client;
                 epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client, &ev);
                 continue;
             }
-            if (events[n].events == EPOLLIN) {
+            if (events[n].events == EPOLLIN && events[n].data.fd != pipefd[0]) {
                 //read
-                int ret_size = TEMP_FAILURE_RETRY(recv(events[n].data.fd, read_buffer, MAX_QUEUE_SIZE, 0));
+                transform_cmd_socket_data_t* recv_data = (transform_cmd_socket_data_t*)read_buffer;
+                recv_data->sock_fd = events[n].data.fd;
+
+                int ret_size = TEMP_FAILURE_RETRY(recv(events[n].data.fd, recv_data->data, MAX_QUEUE_SIZE, 0));
                 if (ret_size == 0) {
                     //断开连接
                     epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[n].data.fd, nullptr);
                     close(events[n].data.fd);
                     std::printf("close connect\n");
+                    continue;
                 }
                 if (ret_size == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
                     //
                     continue;
                 }
-                p_2_c_queue->write(read_buffer, ret_size);
+                recv_data->cmd_size = ret_size + sizeof(transform_cmd_socket_data_t);
+                recv_data->data_len = ret_size;
+                p_2_c_queue->write(read_buffer, recv_data->cmd_size);
+            }
+            if (events[n].events == EPOLLOUT) {
+                continue;
+            }
+            if (events[n].data.fd == pipefd[0]) {
+                //wake up by pipe
             }
         }
-        // unsigned char foo[64] = {0};
-        // int ret_size = recv(client, foo, sizeof(foo), 0);
-        // close(client);
+        //read and discard from the pipe
+        read(pipefd[0], pipe_discard_buffer, 1024);
+        //read from child
+        int ret = c_2_p_queue->read(write_buffer, MAX_QUEUE_SIZE);
+        //handle the cmd from child
+        if (ret == -1) continue;//error happened
+        if (ret < sizeof(transform_cmd_t)) {
+            continue;//should not happened
+        }
+        while (ret) {
+            transform_cmd_t* cmd = (transform_cmd_t*)write_buffer;
+            if (cmd->cmd_type == 0) {
+                //socket data
+                //just echo
+                transform_cmd_socket_data_t* send_data = (transform_cmd_socket_data_t*)write_buffer;
+                int ret = send(send_data->sock_fd, send_data->data, send_data->data_len, 0);//TODO(dmy) 非阻塞io，需要处理一下
+                if (ret == -1) {
+                    perror("send");
+                }
+            }
+
+            ret = ret >= cmd->cmd_size ? ret - cmd->cmd_size : 0;
+        }
     }
     close(server_fd);
-    // p_2_c_queue->write(foo, ret_size);
     free(read_buffer);
+    free(pipe_discard_buffer);
+    free(write_buffer);
 }
 
 // unsigned char read_buffer_queue[MAX_QUEUE_SIZE];
 void child()
 {
-    void *read_buffer = malloc(MAX_QUEUE_SIZE);
+    void *read_buffer = malloc(MAX_QUEUE_SIZE * 2);
+    void *write_buffer = malloc(MAX_QUEUE_SIZE * 2);
     memset(read_buffer, 0, MAX_QUEUE_SIZE);
+
+    char pipe_discard = 'a';
     while(true)
     {
         //TODO(dmy)修改为 fixed timestamp
@@ -180,10 +242,29 @@ void child()
         int ret = p_2_c_queue->read(read_buffer, MAX_QUEUE_SIZE);
         if (ret == -1) break;//error handle
         if (ret) {
-            std::string s((char*)read_buffer, ret);
-            std::printf("%s\n", s.c_str());
+            if (ret < sizeof(transform_cmd_t)) {
+                break;//should not happened
+            }
+
+            while (ret) {
+                transform_cmd_t* cmd = (transform_cmd_t*)read_buffer;
+                if (cmd->cmd_type == 0) {
+                    //socket data
+                    //just echo
+                    transform_cmd_socket_data_t* recv_data = (transform_cmd_socket_data_t*)read_buffer;
+                    transform_cmd_socket_data_t* send_data = (transform_cmd_socket_data_t*)write_buffer;
+                    memcpy(send_data, recv_data, recv_data->cmd_size);
+                    c_2_p_queue->write(send_data, send_data->cmd_size);
+                    write(pipefd[1], &pipe_discard, sizeof(pipe_discard));
+                    std::string s((char*)recv_data->data, recv_data->data_len);
+                    std::printf("recv_dat:%s\n", s.c_str());
+                }
+
+                ret = ret >= cmd->cmd_size ? ret - cmd->cmd_size : 0;
+            }
             // break;
         }
     }
     free(read_buffer);
+    free(write_buffer);
 }
--- testcli.py
+++ testcli.py
@@ -4,14 +4,10 @@ import select
 try:
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.connect(('127.0.0.1', 8890))
-	client.setblocking(0)
+	# client.setblocking(0)
 	# for i in range(1,3):
 	client.sendall("hello world")
-		# try:
-			# recv_data = client.recv(1024)
-			# print recv_data
-		# except socket.error:
-			# pass
-		# time.sleep(100000)
+	recv_data = client.recv(1024)
+	print recv_data
 finally:
 	client.close()
