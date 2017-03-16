b718cf6286d5ea0222d8d81e6a944d5e1934767a
dmy <dmy@debian.debian>
2017-03-16 17:03:33 +0800

    add queue(lock type)

--- main.cpp
+++ main.cpp
@@ -1,60 +1,93 @@
 #include <cstdio>
 #include <unistd.h>
-//test multi process mutex
 #include <pthread.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/mman.h>
 #include <sys/socket.h>
-// #include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <fcntl.h>
 #include <string.h>
 #include <string>
 #include <iostream>
-// #include <sys/types.h>
-pthread_mutex_t* mutex;
-void* shm_buffer;
 
-struct io_data_t
+#define MAX_QUEUE_SIZE 1024 * 1024 * 64
+struct transform_queue_t
 {
-    uint32_t data_len;
-    unsigned char* data[0];
+    pthread_mutex_t mutex;
+    uint32_t idx;
+    unsigned char queue_data[0];
+    void init()
+    {
+        idx = 0;
+        pthread_mutexattr_t attr;
+        pthread_mutexattr_init(&attr);
+        int err = pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
+        if (err) perror("no support for shared mutex");
+        pthread_mutex_init(&mutex, &attr);
+        pthread_mutexattr_destroy(&attr);
+    }
+
+    void final()
+    {
+        pthread_mutex_destroy(&mutex);
+    }
+
+    int read(void* buffer, uint32_t buffer_len)
+    {
+        pthread_mutex_lock(&mutex);
+        if (!idx) {
+            pthread_mutex_unlock(&mutex);
+            return 0;
+        }
+
+        if (buffer_len <= MAX_QUEUE_SIZE - idx) {
+            pthread_mutex_unlock(&mutex);
+            return -1;//bufferlen需要大于队列最大长度
+        }
+        uint32_t copy_len = idx;
+        memcpy(buffer, queue_data, idx);
+        idx = 0;
+        pthread_mutex_unlock(&mutex);
+        return copy_len;
+    }   
+
+    bool write(void* data, uint32_t data_len)
+    {
+        pthread_mutex_lock(&mutex);
+        if (data_len + idx >= MAX_QUEUE_SIZE) {
+            pthread_mutex_unlock(&mutex);
+            return false;//空间不足，交给上层逻辑处理
+        }
+
+        memcpy(queue_data + idx, data, data_len);
+        idx += data_len;
+        pthread_mutex_unlock(&mutex);
+        return true;
+    }
 };
 
+transform_queue_t* p_2_c_queue;
+transform_queue_t* c_2_p_queue;
+unsigned char read_buffer[MAX_QUEUE_SIZE];
 void parent();
 void child();
 int main()
 {
-    mutex = (pthread_mutex_t*)mmap(nullptr, sizeof(pthread_mutex_t), PROT_READ|PROT_WRITE,
-        MAP_SHARED|MAP_ANON, 0, 0);
-    shm_buffer = mmap(nullptr, 1024, PROT_READ|PROT_WRITE,
-        MAP_SHARED|MAP_ANON, 0, 0);
-    memset(shm_buffer, 0, 1024);
+    p_2_c_queue = (transform_queue_t*)mmap(nullptr, MAX_QUEUE_SIZE + sizeof(transform_queue_t), 
+        PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANON, 0, 0);
+    c_2_p_queue = (transform_queue_t*)mmap(nullptr, MAX_QUEUE_SIZE + sizeof(transform_queue_t), 
+        PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANON, 0, 0);
+    p_2_c_queue->init();
+    c_2_p_queue->init();
 
-    pthread_mutexattr_t attr;
-    pthread_mutexattr_init(&attr);
-    int err = pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
-    if (err) perror("");
-    pthread_mutex_init(mutex, &attr);
-    pthread_mutexattr_destroy(&attr);
     int pid = fork();
     if (pid) {
-        //父进程锁住，等十秒
-        // pthread_mutex_lock(mutex);
-        // std::printf("呵呵思密达\n");
-        // sleep(10);
-        // pthread_mutex_unlock(mutex);
-        // wait(nullptr);
-        // sleep(1);//
         parent();
+        wait(nullptr);
     } else {
         child();
-        // sleep(1);//
-        // pthread_mutex_lock(mutex); 
-        // std::printf("wtf!!\n");
-        // pthread_mutex_unlock(mutex);
     }
     return 0;
 }
@@ -79,33 +112,26 @@ void parent()
     }
 
     int client = accept(socketfd, nullptr, nullptr);
-    char foo[64] = {0};
-    recv(client, foo, sizeof(foo), 0);
-    // printf("%s\n", foo);
+    unsigned char foo[64] = {0};
+    int ret_size = recv(client, foo, sizeof(foo), 0);
     close(client);
     close(socketfd);
 
-    pthread_mutex_lock(mutex);
-    io_data_t* trans_data = (io_data_t*)shm_buffer;
-    trans_data->data_len = strlen(foo);
-    memcpy(trans_data->data, foo, trans_data->data_len);
-    pthread_mutex_unlock(mutex);
+    p_2_c_queue->write(foo, ret_size);
 }
 
 void child()
 {
+    memset(read_buffer, 0, MAX_QUEUE_SIZE);
     while(true)
     {
         usleep(1000);
-        if (pthread_mutex_trylock(mutex) == 0) {
-            io_data_t* trans_data = (io_data_t*)shm_buffer;
-            if (trans_data->data_len) {
-                std::string s((char*)trans_data->data, trans_data->data_len);
-                std::printf("%s\n", s.c_str());
-                pthread_mutex_unlock(mutex);
-                break;
-            }
-            pthread_mutex_unlock(mutex);
+        int ret = p_2_c_queue->read(read_buffer, MAX_QUEUE_SIZE);
+        if (ret == -1) return;//error handle
+        if (ret) {
+            std::string s((char*)read_buffer, ret);
+            std::printf("%s\n", s.c_str());
+            return;
         }
     }
 }
