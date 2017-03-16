8ceef4a8bcbc578bdce5550961551945d33ff2d8
dmy <dmy@debian.debian>
2017-03-16 18:45:48 +0800

    add epoll loop

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@
 #include <string.h>
 #include <string>
 #include <iostream>
-
+#include <sys/epoll.h>
 #define MAX_QUEUE_SIZE 1024 * 1024 * 64
+#define MAX_EPOLL_EVENT 128 
 struct transform_queue_t
 {
     pthread_mutex_t mutex;
@@ -70,7 +71,7 @@ struct transform_queue_t
 
 transform_queue_t* p_2_c_queue;
 transform_queue_t* c_2_p_queue;
-unsigned char read_buffer[MAX_QUEUE_SIZE];
+
 void parent();
 void child();
 int main()
@@ -92,46 +93,97 @@ int main()
     return 0;
 }
 
-void parent()
+//TODO(dmy) 非阻塞的server socket和阻塞的server socket的区别
+//TODO(dmy) 设置发送接收缓存 或者交给os，这里只读取一下。
+int get_server_socket(const std::string& addr, int port)
 {
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in serveraddr;
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(8890);
+    serveraddr.sin_port = htons(port);
 
-    char addr[] = "127.0.0.1";
-    inet_pton(AF_INET, addr, &serveraddr.sin_addr.s_addr);
+    inet_pton(AF_INET, addr.c_str(), &serveraddr.sin_addr.s_addr);
     int reuse = 1;
     setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
     bind(socketfd, (const sockaddr*)&serveraddr, sizeof(serveraddr));
     int ret = listen(socketfd, 20);
     if (ret) {
         perror("listen");
-        return;
+        return 0;
     }
-
-    int client = accept(socketfd, nullptr, nullptr);
-    unsigned char foo[64] = {0};
-    int ret_size = recv(client, foo, sizeof(foo), 0);
-    close(client);
-    close(socketfd);
-
-    p_2_c_queue->write(foo, ret_size);
+    return socketfd;
 }
 
+// int set_client_socket(int socket_fd)
+// {
+
+// }
+
+void parent()
+{
+    void* read_buffer = malloc(MAX_QUEUE_SIZE);
+    int server_fd = get_server_socket("127.0.0.1", 8890);
+
+    struct epoll_event ev, events[MAX_EPOLL_EVENT];
+    int epoll_fd = epoll_create(1);//args is ignored,just greater than 0
+    ev.events = EPOLLIN;
+    ev.data.fd = server_fd;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev);
+
+    int nfds = 0;
+    for (;;) {
+        nfds = epoll_wait(epoll_fd, events, MAX_EPOLL_EVENT, 1);//1ms的超时，做一些定时操作
+        for (int n = 0; n < nfds; ++n) {
+            if (events[n].data.fd == server_fd) {
+                int client = accept(server_fd, nullptr, nullptr);
+                fcntl(client, F_SETFL, O_NONBLOCK);
+                ev.events = EPOLLIN;
+                ev.data.fd = client;
+                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client, &ev);
+                continue;
+            }
+            if (events[n].events == EPOLLIN) {
+                //read
+                int ret_size = TEMP_FAILURE_RETRY(recv(events[n].data.fd, read_buffer, MAX_QUEUE_SIZE, 0));
+                if (ret_size == 0) {
+                    //断开连接
+                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[n].data.fd, nullptr);
+                    close(events[n].data.fd);
+                    std::printf("close connect\n");
+                }
+                if (ret_size == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+                    //
+                    continue;
+                }
+                p_2_c_queue->write(read_buffer, ret_size);
+            }
+        }
+        // unsigned char foo[64] = {0};
+        // int ret_size = recv(client, foo, sizeof(foo), 0);
+        // close(client);
+    }
+    close(server_fd);
+    // p_2_c_queue->write(foo, ret_size);
+    free(read_buffer);
+}
+
+// unsigned char read_buffer_queue[MAX_QUEUE_SIZE];
 void child()
 {
+    void *read_buffer = malloc(MAX_QUEUE_SIZE);
     memset(read_buffer, 0, MAX_QUEUE_SIZE);
     while(true)
     {
+        //TODO(dmy)修改为 fixed timestamp
         usleep(1000);
         int ret = p_2_c_queue->read(read_buffer, MAX_QUEUE_SIZE);
-        if (ret == -1) return;//error handle
+        if (ret == -1) break;//error handle
         if (ret) {
             std::string s((char*)read_buffer, ret);
             std::printf("%s\n", s.c_str());
-            return;
+            // break;
         }
     }
+    free(read_buffer);
 }
