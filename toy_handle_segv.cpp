9f31c23b85f19266429627097b3595f4e16c977e
dmy <dmy@debian.debian>
2017-03-22 10:11:10 +0800

    toy to restore from segv

--- toy_handle_segv.cpp
+++ toy_handle_segv.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <signal.h>
+#include <ucontext.h>
+#include <errno.h>
+#include <string.h>
+ucontext_t main_context;
+char stack_buffer[64 * 1024];
 
-int sig_handler
+void sig_handler(int signum)
+{
+    std::printf("sig catch:%d, %s\n", signum, strerror(errno));
+    std::printf("try to restore context\n");
+    std::printf("restore success, continue type your cmd\n");
+    setcontext(&main_context);
+}
 
 int dispatcher(int type)
 {
@@ -14,9 +26,19 @@ int dispatcher(int type)
 
 int main()
 {
+    struct sigaction act;
+    act.sa_handler = sig_handler;
+    sigset_t sigmask;
+    sigemptyset(&sigmask);
+    sigaddset(&sigmask, SIGSEGV);
+    sigaction(SIGSEGV, &act, nullptr);
+    
     int cmd_type;
     std::cout<<"type 0 exit, type 1 print 1, type 2 generate segv"<<std::endl;
     for(;;) {
+        getcontext(&main_context);
+        main_context.uc_stack.ss_sp = stack_buffer;
+        main_context.uc_stack.ss_size = sizeof(stack_buffer);
         std::cin >> cmd_type;
         if (cmd_type == 0) return 0;
         dispatcher(cmd_type);
